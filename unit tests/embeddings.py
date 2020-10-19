8512791053394e665aed98f8b81f238eced686c8
nir schipper <32059843+nirsc@users.noreply.github.com>
2020-10-19 09:48:29 +0300

    Delete embeddings.py

--- unit tests/embeddings.py
+++
@@ -1,8 +0,0 @@
-from utils.embeddings import *
-import pandas as pd
-
-
-DATA_DIR = '..//data//'
-filename = 'quora_duplicate_questions.tsv'
-df = pd.read_csv(DATA_DIR+filename,delimiter='\t')
-print(df.head())
